03f43f9ee70a342455d216cb27701e2484237813
Nhật Minh <dev@dhnm.eu>
2024-04-14 02:31:36 +0100

    Change color scheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,28 @@ import type { PluginUtils } from "tailwindcss/types/config"
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#0069D0",
+          secondary: "#696979",
+          accent: "#D5A400",
+          neutral: "#24252E",
+          "base-100": "#FFFFFF",
+        },
+      },
+      {
+        dark: {
+          primary: "#84AAFF",
+          secondary: "#AAAAB9",
+          accent: "#866600",
+          neutral: "#F0F0F5",
+          "base-100": "#24252E",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {
       typography: ({ theme }: PluginUtils) => ({
@@ -25,19 +47,18 @@ export default {
         heading: ['"Fira Sans"', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        congress: {
-          "50": "oklch(98.18% 0.03 157.40 / <alpha-value>)",
-          "100": "oklch(96.12% 0.06 161.35 / <alpha-value>)",
-          "200": "oklch(93.69% 0.10 163.88 / <alpha-value>)",
-          "300": "oklch(90.84% 0.14 164.57 / <alpha-value>)",
-          "400": "oklch(89.41% 0.17 169.51 / <alpha-value>)",
-          "500": "oklch(90.06% 0.16 186.72 / <alpha-value>)",
-          "600": "oklch(81.60% 0.15 216.50 / <alpha-value>)",
-          "700": "oklch(69.81% 0.17 243.77 / <alpha-value>)",
-          "800": "oklch(55.56% 0.18 253.81 / <alpha-value>)",
-          "900": "oklch(39.56% 0.13 254.79 / <alpha-value>)",
-          DEFAULT: "oklch(39.56% 0.13 254.79 / <alpha-value>)",
-          "950": "oklch(29.91% 0.11 260.76 / <alpha-value>)",
+        science: {
+          "100": "#FFFFFF",
+          "200": "#ECF0FF",
+          "300": "#C5D4FF",
+          "400": "#84AAFF",
+          "500": "#498FFF",
+          "600": "#007AF0",
+          "700": "#0069D0",
+          DEFAULT: "#0069D0",
+          "800": "#0051A2",
+          "900": "#003A79",
+          "950": "#002451",
         },
       },
     },
