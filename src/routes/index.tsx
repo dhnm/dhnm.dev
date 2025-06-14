b568940f7f106d6b543ec9e850c2567933a21ca9
Nhật Minh <mail@dhnm.dev>
2025-06-14 21:09:33 +0200

    Add logo, fonts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,22 @@
 import { component$ } from "@builder.io/qwik"
 import ContactForm from "~/components/index/ContactForm"
 
+import Logo from "~/assets/dhnm_logo.png?quality=100&jsx"
+
 export default component$(() => {
   return (
     <div class="m-auto max-w-md">
-      <div class="mt-12 mb-6">
-        <h1 class="my-6">Hey there,</h1>
-        <p class="my-3">
-          I am Nhật Minh and I explore ways of utilizing technology to make our
-          lives more fun and efficient.
+      <Logo
+        class="mx-auto my-12 h-24 w-24"
+        alt="Logo - a black circle and right-angle shape on white circular background"
+      />
+      <div class="mt-6 mb-9 px-2 text-lg">
+        <h2 class="mb-6">Hey there,</h2>
+        <p class="mb-3">
+          I am Nhật Minh. I research and build technologies that help people
+          perform their tasks efficiently and with ease.
         </p>
-        <p class="my-3">
+        <p>
           Feel free to contact me if you have any questions or if you want to
           chat about my work!
         </p>
